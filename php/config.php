7e4605fe3262f333d9e9638665276dbe2e60a344
caloloc2 <calolomino@gmail.com>
2018-05-29 09:56:22 -0500

    actualiza

--- php/config.php
+++ php/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define("HOSTNAME", "127.0.0.1");// Nombre del host (localhost aws instance)
+define("HOSTNAME", "localhost");// Nombre del host (localhost aws instance)
 define("USERNAME", "root"); // Nombre del usuario
-define("PASSWORD", ""); // Contraseña
+define("PASSWORD", "385402292Mica_02"); // Contraseña
 define("DATABASE", "sensores_2"); // Nombre de la base de datos
