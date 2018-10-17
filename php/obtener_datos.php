31ac0085374d069ff362397223061e70dce761cd
caloloc2 <calolomino@gmail.com>
2018-10-17 16:12:48 -0500

    actualiza

--- php/obtener_datos.php
+++ php/obtener_datos.php
@@ -1,23 +1,24 @@
 <?php 
 
-$respuesta['estado'] = false;
+$respuesta['estado'] = false; // setea el estado de la respuesta como falso
 
 try{
-	require 'meta.php';
-	$id_dispositivo = $_POST['id_dispositivo'];
-	$fecha_inicio = $_POST['fecha_inicio'];
-	$fecha_final = $_POST['fecha_final'];
+	require 'meta.php'; // llama al archivo de conexion a base de datos
+	$id_dispositivo = $_POST['id_dispositivo']; // lee el id del dispoitivo
+	$fecha_inicio = $_POST['fecha_inicio']; // lee la fecha de inicio
+	$fecha_final = $_POST['fecha_final']; // lee la fecha final
 
+	// realiza la consulta a base de datos con los campos leidos
 	$aux = Meta::Consulta("SELECT * FROM datos WHERE ((id_dispositivo=".$id_dispositivo.") AND (fecha BETWEEN '".$fecha_inicio."' AND '".$fecha_final."')) ORDER BY id_dato ASC");
 
-	if (count($aux)){
-		$respuesta['datos'] = $aux;
+	if (count($aux)){ // si existe algun dato
+		$respuesta['datos'] = $aux; // guarda el o los datos en la variable respuesta['datos']
 	}
 
-	$respuesta['estado'] = true;
+	$respuesta['estado'] = true; // si todo se ejecuto bien,cambia a verdadero
 	
-}catch(Exception $e){
-	$respuesta['error'] = $e->getMessage();
+}catch(Exception $e){ // controla errores
+	$respuesta['error'] = $e->getMessage(); // si hay errores en respuesta['error'] especifica q error es
 }
 
-echo json_encode($respuesta);
+echo json_encode($respuesta); // escribe en formato json los valores de estado, datos y error(en caso de haber)
